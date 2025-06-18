48d996d20f289d66cf6d670ecccce83e2ca2feb0
hubbiy18 <febie.elfakhrul@gmail.com>
2025-06-18 21:12:48 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,27 +6,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 def main():
-    mlflow.set_experiment("Diabetes Prediction")
+    df = pd.read_csv("diabetes_cleaned.csv")
 
-    with mlflow.start_run():
-        df = pd.read_csv("diabetes_cleaned.csv")
+    X = df.drop("diabetes", axis=1)
+    y = df["diabetes"]
 
-        X = df.drop("diabetes", axis=1)
-        y = df["diabetes"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
 
-        model = RandomForestClassifier()
-        model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")  # Ini akan buat artifacts/model/
 
-        # Log metrics and model
-        mlflow.log_metric("accuracy", acc)
-        mlflow.sklearn.log_model(model, "model")  # Ini akan buat artifacts/model/
-
-        print("Model trained and logged to MLflow.")
+    print("Model trained and logged to MLflow.")
 
 if __name__ == "__main__":
     main()
